d554a5805c9f9bba8c9c0af606db675fcca5b673
Yu Hao Wong <wongyuhao@gmail.com>
2021-09-15 23:57:15 -0700

    implement header

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,8 +1,38 @@
+import Link from 'next/link'
 
 export default function Header({}){
     return (
-        <div>
-            Hello World!
-        </div>
+        <nav className='navbar cl-blue sticky-top navbar-expand-md bg-white px-3'>
+            <div className="container-fluid">
+            <Link href="/">
+                <a className='navbar-brand mx-4'>cledge</a>
+            </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="fs-7 collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                {/* user authenticated */}
+                <div className="navbar-nav">
+                    <Link href="/resources">
+                        <a className="nav-link" href="/login">Resources</a>
+                    </Link>
+                    <Link href="/progress">
+                        <a className="nav-link" href="/login">Progress</a>
+                    </Link>
+                    <Link href="/account">
+                        <a className="nav-link " href="/login">My Account</a>
+                    </Link>
+                    
+                </div>
+                <Link href="/login">
+                        <a className="nav-link" href="/login">Login</a>
+                </Link>
+
+                </div>
+            </div>
+    
+            
+        </nav>
     );
 }
