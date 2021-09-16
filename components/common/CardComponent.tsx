1ad142d3aa7d3284b16af351e21e67f90a96824a
yoyo7373 <yousefgomaa@hotmail.com>
2021-09-16 14:13:17 -0700

    created initial resources page and card component

---
+++ components/common/CardComponent.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+export default function CardComponent() {
+  useEffect(() => {}, []);
+  return (
+    <div className="card-container mt-5">
+      <div className="card-title">SAT Vocabulary III</div>
+      <div></div>
+    </div>
+  );
+}
--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,7 +1,75 @@
-export default function resources (){
-    return(
-        <div>
-            This is the resource page
-        </div>
-    )
+import { useState } from "react";
+import CardComponent from "../components/common/CardComponent";
+
+export default function resources() {
+  const cledgeBlue = "#2651ed";
+  const [currTab, setCurrTab] = useState("resources");
+  return (
+    <div className="d-flex flex-column">
+      <div
+        className="d-flex flex-row align-items-center justify-content-evenly"
+        style={{ width: "35%" }}
+      >
+        <button
+          onClick={() => {
+            setCurrTab("resources");
+          }}
+          className="resources-tab-nav-btn"
+        >
+          <div style={{ width: "fit-content" }}>
+            Resources
+            <div
+              style={{
+                height: "3px",
+                backgroundColor:
+                  currTab === "resources" ? cledgeBlue : "transparent",
+              }}
+            />
+          </div>
+        </button>
+        <button
+          onClick={() => {
+            setCurrTab("articles");
+          }}
+          className="resources-tab-nav-btn"
+        >
+          <div style={{ width: "fit-content" }}>
+            Articles
+            <div
+              style={{
+                height: "3px",
+                backgroundColor:
+                  currTab === "articles" ? cledgeBlue : "transparent",
+              }}
+            />
+          </div>
+        </button>
+        <button
+          onClick={() => {
+            setCurrTab("videos");
+          }}
+          className="resources-tab-nav-btn"
+        >
+          <div style={{ width: "fit-content" }}>
+            Videos
+            <div
+              style={{
+                height: "3px",
+                backgroundColor:
+                  currTab === "videos" ? cledgeBlue : "transparent",
+              }}
+            />
+          </div>
+        </button>
+      </div>
+      <div className="align-self-center d-flex flex-row flex-wrap w-100 justify-content-evenly">
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+      </div>
+    </div>
+  );
 }
