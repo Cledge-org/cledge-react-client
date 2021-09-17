790df56e9050fe19bfafb974c9a51c1dd80cc1c6
jollypolly123 <rachelle.hu123@gmail.com>
2021-09-17 03:20:17 -0400

    footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/common/Footer";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -63,19 +64,7 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/* <Footer/> */}
     </div>
   );
 };
