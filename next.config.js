b58e9351b2446d2944b7da5b8e70b0b90c88ff6c
Yu Hao Wong <wongyuhao@gmail.com>
2021-09-17 17:39:41 -0700

    add env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config()
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID,
+    AUTH_CLIENT_SECRET: process.env.AUTH_CLIENT_SECRET,
+    AUTH_TENANT_NAME: process.env.AUTH_TENANT_NAME,
+    AUTH_TENANT_GUID: process.env.AUTH_TENANT_GUID,
+    JWT_SECRET: process.env.JWT_SECRET,
+    USER_FLOW: process.env.USER_FLOW,
+  }
 }
