759da381884404cca00dad9ba341604f89307c6b
yoyo7373 <yousefgomaa@hotmail.com>
2021-09-20 13:08:18 -0700

    started the progress page

--- pages/progress.tsx
+++ pages/progress.tsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faSortDown } from "@fortawesome/free-solid-svg-icons";
+
 //profile progress/ question summary page
-export default function Progress (){
-    return(
-        <div>
-            This is the question summary page
-        </div>
-    )
+export default function Progress() {
+  return (
+    <div className="container-fluid vh-100 d-flex flex-row px-0">
+      <div className="d-flex flex-column bg-light-gray" style={{ flex: 1 }}>
+        <DropDownTab />
+        <DropDownTab />
+      </div>
+      <div className="d-flex" style={{ flex: 3 }}></div>
+    </div>
+  );
 }
 
-Progress.requireAuth = true;
+function DropDownTab() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className="progress-dropdown-container">
+      <button
+        className="progress-dropdown-btn"
+        onClick={() => {
+          setIsExpanded(!isExpanded);
+        }}
+      >
+        <div>Extracurriculars</div>
+        <FontAwesomeIcon icon={faSortDown} color="#000000" size="xs" />
+      </button>
+      <div
+        className={
+          isExpanded
+            ? "progress-dropdown-menu-expanded"
+            : "progress-dropdown-menu-closed"
+        }
+      >
+        <button className="progress-dropdown-menu-btn">Hello</button>
+        <button className="progress-dropdown-menu-btn">Hello 2</button>
+      </div>
+    </div>
+  );
+}
+
+Progress.requireAuth = false;
