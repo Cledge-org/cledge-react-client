8cd11331867a2b70ef5e2a409c901b8142b00671
Bryan Lim <bryanlim@cs.washington.edu>
2021-11-02 16:01:24 -0700

    Resource route

--- pages/api/get-resources-info.ts
+++ pages/api/get-resources-info.ts
@@ -1,4 +1,6 @@
+import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
+import assert from "assert";
 
 export const config = {
   api: {
@@ -7,14 +9,37 @@ export const config = {
 };
 
 export default async (req: NextApiRequest, resolve: NextApiResponse) => {
-  return resolve.status(200).send(getResourcesInfo("TEST_USER_ID"));
+  return resolve.status(200).send(await getResourcesInfo());
 };
 
-export async function getResourcesInfo(userId: string): Promise<ResourcesInfo> {
-  const dummyData: Promise<ResourcesInfo> = Promise.resolve({
-    resources: [{ source: "", title: "SAT Vocabulary III" }],
-    articles: [{ source: "", title: "", description: "" }],
-    videoList: [{ source: "", title: "" }],
+export const getResourcesInfo = async (): Promise<ResourcesInfo> => {
+  return new Promise((res, err) => {
+    MongoClient.connect(
+      MONGO_CONNECTION_STRING,
+      async (connection_err, client) => {
+        assert.equal(connection_err, null);
+        const resource_db = client.db("resources");
+        const videoList: CardVideo[] = (
+          await resource_db.collection("videos").find().toArray()
+        ).map((x) => {
+          return { source: x.source, title: x.title };
+        });
+        const articles: CardArticle[] = (
+          await resource_db.collection("articles").find().toArray()
+        ).map((x) => {
+          return {
+            description: x.description,
+            source: x.source,
+            title: x.title,
+          };
+        });
+        const resources: CardResource[] = (
+          await resource_db.collection("resources").find().toArray()
+        ).map((x) => {
+          return { source: x.source, title: x.title };
+        });
+        res({ videoList, articles, resources });
+      }
+    );
   });
-  return dummyData;
-}
+};
