2e7c9564329ad77292f8d09302d9123ab2bccf68
Bryan Lim <bryanlim@cs.washington.edu>
2021-11-06 15:50:24 -0700

    Create update-user.ts

---
+++ pages/api/update-user.ts
@@ -0,0 +1,56 @@
+import { MongoClient } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import assert from "assert";
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
+
+export default async (req: NextApiRequest, resolve: NextApiResponse) => {
+  const { id, name, address, grade, birthday, email } = req.body;
+  if (!id) {
+    resolve.status(400).send("User ID required (id)");
+  } else {
+    try {
+      await updateUser(id, {
+        name,
+        address,
+        grade,
+        birthday,
+        email,
+      });
+      resolve.status(200).send("Success");
+    } catch (e) {
+      resolve.status(500).send(e);
+    }
+  }
+};
+
+export const updateUser = async (
+  id: string,
+  user: AccountInfo
+): Promise<void> => {
+  return new Promise((res, err) => {
+    MongoClient.connect(
+      MONGO_CONNECTION_STRING,
+      async (connection_err, client) => {
+        assert.equal(connection_err, null);
+        const user_db = client.db("users");
+        const users_collection = user_db.collection("users");
+        try {
+          // Remove undefined and null fields in user as to not delete them
+          for (const propName in user) {
+            if (user[propName] === null || user[propName] === undefined)
+              delete user[propName];
+          }
+          await users_collection.updateOne({ _id: id }, { $set: user });
+          res();
+        } catch (e) {
+          err(e);
+        }
+      }
+    );
+  });
+};
