b2d6a10ff780564a13a5798930db10afeac1ab74
yoyo7373 <yousefgomaa@hotmail.com>
2022-01-26 09:55:38 -0800

    made initial deployment but serverless functions aren't working figuring out how to use devops to make them work

--- config.ts
+++ config.ts
@@ -1,6 +1,3 @@
 export const MONGO_CONNECTION_STRING =
   "mongodb://cledge-db:Tc7PYDfYtB1jBrPfBDPYSNLhlovMKU6AatyKbWPAE75IFy1tLEsczdULMYfaoiFu5bH9qbna704PQCdHBKT8YQ==@cledge-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@cledge-db@";
-export const ORIGIN_URL =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
-    : "https://cledge-dev-functions.azurewebsites.net";
+export const ORIGIN_URL = "https://cledge-dev.azurewebsites.net";
