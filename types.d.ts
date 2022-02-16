645ce287820aa41404b5452d11dad7e651f09581
Bryan Lim <bryanlim@cs.washington.edu>
2022-02-16 00:57:54 -0800

    Make _id optional, re-add Dashboard type

--- types.d.ts
+++ types.d.ts
@@ -40,7 +40,7 @@ interface CardResource {
 
 //Progress Page Types -->
 interface UserProgress_Db extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   firebaseId: string;
   responses: UserResponse[];
 }
@@ -57,22 +57,22 @@ interface ProgressInfo {
   questionData: QuestionList[];
 }
 interface QuestionList extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   chunks: QuestionChunk[];
 }
 interface QuestionList_Db extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   chunks: string[]; // Document IDs of chunks
 }
 interface QuestionChunk extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   questions: Question[];
 }
 interface QuestionChunk_Db extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   questions: ObjectId[]; // Document IDs of question data
 }
@@ -87,12 +87,17 @@ interface Question extends WithId<Document> {
 }
 
 //Learning Pathways Types -->
+interface Dashboard {
+  userName: string;
+  userTags: string[];
+  userProgress: PathwayProgress[];
+  checkIns: string[];
+}
 interface UserPathway {
   pathway: Course;
   userTags: string[];
   userCourseProgress: PathwayProgress;
 }
-
 interface PathwayProgress {
   pathwayId: string; // ID of the pathway this progress belongs to, NOT database ID of the progress itself
   finished: boolean;
@@ -111,26 +116,26 @@ interface ContentProgress {
   videoTime: number;
 }
 interface Pathway {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   modules: PathwayModule[];
   tags: string[];
 }
 interface Pathway_Db extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   tags: string[];
   modules: ObjectId[]; // Module document IDs
   name: string;
 }
 interface PathwayModule {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   presetContent: PresetContent[];
   personalizedContent: PersonalizedContent[];
   tags: string[];
 }
 interface PathwayModule_Db extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   presetContent: PresetContent[];
   tags: string[];
@@ -143,7 +148,7 @@ interface PresetContent {
   content?: string;
 }
 interface PersonalizedContent extends WithId<Document> {
-  _id: ObjectId;
+  _id?: ObjectId;
   moduleId: ObjectId;
   priority: number;
   tags: string[];
