6ae7d45ccc0e7b99f99352776150560af632c90d
yoyo7373 <yousefgomaa@hotmail.com>
2022-02-22 11:22:17 -0800

    created basic tabs and filtering mainly just an outline no styling yet ran out of time today

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -8,6 +8,8 @@ import CardImage from "../components/common/Card_Image";
 import { GetServerSidePropsContext } from "next";
 import { getResourcesInfo } from "./api/get-resources";
 import { ORIGIN_URL } from "../config";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   try {
@@ -28,33 +30,41 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 const Resources: NextApplicationPage<{ resourcesInfo: ResourcesInfo }> = ({
   resourcesInfo,
 }) => {
-  const [currTab, setCurrTab] = useState("resources");
+  const resourceTypes = [
+    "All",
+    "Extracurricular",
+    "Essay",
+    "Application",
+    "Standardized Tests",
+    "Academics",
+    "Grades",
+    "Scholarship",
+  ];
+  const [currTab, setCurrTab] = useState("all");
   return (
     <div className="d-flex flex-column vh-100">
       <div className="row">
-        <TabButton
-          onClick={() => {
-            setCurrTab("resources");
-          }}
-          title="Resources"
-          currTab={currTab}
-        />
-        <TabButton
-          onClick={() => {
-            setCurrTab("articles");
-          }}
-          title="Articles"
-          currTab={currTab}
-        />
-        <TabButton
-          onClick={() => {
-            setCurrTab("videos");
-          }}
-          title="Videos"
-          currTab={currTab}
-        />
+        {resourceTypes.map((type) => {
+          return (
+            <TabButton
+              onClick={() => {
+                setCurrTab(type.toLowerCase());
+              }}
+              title={type}
+              currTab={currTab}
+            />
+          );
+        })}
+        <button>Filter</button>
       </div>
-
+      {currTab === "all" ? (
+        <div>
+          <div>
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+          <input />
+        </div>
+      ) : null}
       <div className="container-fluid align-self-center mx-0 col justify-content-evenly">
         {currTab === "resources" ? (
           <div className="row jusify-content-evenly">
