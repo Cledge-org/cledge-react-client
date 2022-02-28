54c38334ce0ed91557af295ec47ae906b7f58a19
jakob-bjorner <jbjorner3@gatech.edu>
2022-02-28 04:27:20 -0500

    ensure key props are used properly and enforced. prototype essay tool

--- pages/essay.js
+++
@@ -1,7 +0,0 @@
-function essay() {
-  return <div>essay pathway to do</div>;
-
-}
-// remember to check that they use next Links instead of other things.
-// The app can store state across pages.
-export default essay;
