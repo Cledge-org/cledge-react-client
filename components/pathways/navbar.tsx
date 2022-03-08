c4ff1ee37e187a21e189f16e204df36cbe703d59
jakob-bjorner <jbjorner3@gatech.edu>
2022-03-08 02:13:54 -0500

    init typed pathways

---
+++ components/pathways/navbar.tsx
@@ -0,0 +1,17 @@
+import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
+import {
+  faChevronDown,
+  faFileAlt,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+
+
+function navbar() {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
