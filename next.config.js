80a54ba1204346078f3f72d0e44c680effd3fd3d
yousefg <yousefgomaa@hotmail.com>
2022-05-19 00:06:45 -0700

    working on fixing styling issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 require("dotenv").config();
-
+const regexEqual = (x, y) => {
+  return (
+    x instanceof RegExp &&
+    y instanceof RegExp &&
+    x.source === y.source &&
+    x.global === y.global &&
+    x.ignoreCase === y.ignoreCase &&
+    x.multiline === y.multiline
+  );
+};
 module.exports = {
   reactStrictMode: true,
   eslint: {
@@ -15,5 +24,24 @@ module.exports = {
     USER_FLOW_AUTH: process.env.USER_FLOW_AUTH,
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+  },
+  webpack: (config) => {
+    const oneOf = config.module.rules.find(
+      (rule) => typeof rule.oneOf === "object"
+    );
+    if (oneOf) {
+      const moduleCssRule = oneOf.oneOf.find((rule) =>
+        regexEqual(rule.test, /\.module\.scss$/)
+      );
+      if (moduleCssRule) {
+        const cssLoader = moduleCssRule.use.find(({ loader }) =>
+          loader.includes("sass-loader")
+        );
+        if (cssLoader) {
+          cssLoader.options.modules.mode = "local";
+        }
+      }
+    }
+    return config;
   },
 };
