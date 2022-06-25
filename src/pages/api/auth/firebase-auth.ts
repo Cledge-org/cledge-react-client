7464810e2c4525376288f0a41e36918e61f9102a
yousefg <yousefgomaa@hotmail.com>
2022-06-25 16:19:42 -0700

    temporarily disabled admin authentication to see if it fixes deployment

--- src/pages/api/auth/firebase-auth.ts
+++ src/pages/api/auth/firebase-auth.ts
@@ -13,30 +13,30 @@ import {
   getIdTokenResult,
 } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { getAuth as getAdminAuth } from "firebase-admin/auth";
-import {
-  getApp,
-  initializeApp as initializeAdminApp,
-} from "firebase-admin/app";
+// import { getAuth as getAdminAuth } from "firebase-admin/auth";
+// import {
+//   getApp,
+//   initializeApp as initializeAdminApp,
+// } from "firebase-admin/app";
 
 const firebaseCreds = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
-const getAdminApp = (appName?: string) => {
-  try {
-    return getApp(appName);
-  } catch (err) {
-    return undefined;
-  }
-};
+// const getAdminApp = (appName?: string) => {
+//   try {
+//     return getApp(appName);
+//   } catch (err) {
+//     return undefined;
+//   }
+// };
 // console.error(firebaseCreds);
-export const firebaseApp = initializeApp(firebaseCreds);
-export const firebaseAuth = getAuth(firebaseApp);
-const firebaseAdminAuth = getAdminAuth(
-  getAdminApp(firebaseApp.name) ?? initializeAdminApp(firebaseCreds)
-);
+const firebaseApp = initializeApp(firebaseCreds);
+const firebaseAuth = getAuth(firebaseApp);
+// const firebaseAdminAuth = getAdminAuth(
+//   getAdminApp(firebaseApp.name) ?? initializeAdminApp(firebaseCreds)
+// );
 const provider = new GoogleAuthProvider();
 class AuthFunctions {
   static async signInEmail(email: string, password: string) {
@@ -48,7 +48,7 @@ class AuthFunctions {
         return signInWithEmailAndPassword(firebaseAuth, email, password);
       });
       let idToken = await user.user.getIdToken();
-      await this.validateAdmin(idToken);
+      //await this.validateAdmin(idToken);
       return user.user;
     } catch (err) {
       console.error(err);
@@ -89,34 +89,34 @@ class AuthFunctions {
       console.error(err);
     });
   }
-  static async validateAdmin(idToken: string | undefined) {
-    if (!idToken) {
-      idToken = await getAuth().currentUser.getIdToken(true);
-    }
-    let uid;
-    // Verify the ID token first
-    const claims = await firebaseAdminAuth.verifyIdToken(idToken);
-    if (claims.admin === true) {
-      // Allow access to requested admin resource
-      uid = claims.uid;
-    } else {
-      return false;
-    }
+  // static async validateAdmin(idToken: string | undefined) {
+  //   if (!idToken) {
+  //     idToken = await getAuth().currentUser.getIdToken(true);
+  //   }
+  //   let uid;
+  //   // Verify the ID token first
+  //   const claims = await firebaseAdminAuth.verifyIdToken(idToken);
+  //   if (claims.admin === true) {
+  //     // Allow access to requested admin resource
+  //     uid = claims.uid;
+  //   } else {
+  //     return false;
+  //   }
 
-    console.error(uid);
-    // Lookup the user associated with the specified uid
-    await firebaseAdminAuth.getUser(uid).then((userRecord) => {
-      // Claims can be accessed on the user record
-      console.log(userRecord.customClaims["admin"]);
-    });
-    return true;
-  }
+  //   console.error(uid);
+  //   // Lookup the user associated with the specified uid
+  //   await firebaseAdminAuth.getUser(uid).then((userRecord) => {
+  //     // Claims can be accessed on the user record
+  //     console.log(userRecord.customClaims["admin"]);
+  //   });
+  //   return true;
+  // }
 
-  static async setAdminPriviledge(isAdmin: boolean) {
-    // Set admin priviledge
-    let uid = firebaseAuth.currentUser.uid;
-    await firebaseAdminAuth.setCustomUserClaims(uid, { admin: isAdmin });
-  }
+  // static async setAdminPriviledge(isAdmin: boolean) {
+  //   // Set admin priviledge
+  //   let uid = firebaseAuth.currentUser.uid;
+  //   await firebaseAdminAuth.setCustomUserClaims(uid, { admin: isAdmin });
+  // }
 }
 
 export default AuthFunctions;
