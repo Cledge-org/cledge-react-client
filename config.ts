7887b25cab2e801e1c1feb7e47c85e48d436d772
yousefg <yousefgomaa@hotmail.com>
2022-07-13 12:14:57 -0700

    updated env variables

--- config.ts
+++ config.ts
@@ -1,7 +1,3 @@
-process.env.NEXTAUTH_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://cledge.org"
-    : "http://localhost:3000";
 export const actions = {
   NEW_QUESTION_RESPONSE: "newQuestionResponse",
   UDPATE_QUESTION_RESPONSE: "updateQuestionResponse",
