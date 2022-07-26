9594bd568ec4e977e6b6190dfedcb1dd198f32d0
Chandan <chandancvp08@gmail.com>
2022-07-26 08:32:00 +0530

    made sure session is not undefined while fetching data

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,24 +1,27 @@
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
+import LoadingScreen from "src/common/components/Loading/Loading";
 import { NextApplicationPage } from "src/main-pages/AppPage/AppPage";
 import DashboardPage from "src/main-pages/DashboardPage/DashboardPage";
 
 const Dashboard = () => {
-  const {data :session} = useSession()
+  const { data: session } = useSession()
   const [dashboardParts, setDashboardParts] = useState()
- useEffect(()=>{
-getDashboardData()
- },[])
- async function getDashboardData() {
-const response = await fetch(`/api/get-dashboard-parts?userID=${session.user.uid}`)
-const responseJson = await response.json()
-setDashboardParts(responseJson)
-}
-if (dashboardParts){
-  return <DashboardPage dashboardParts={dashboardParts}/>
-}
-else{
-  return <div>loading</div>
-}
+  useEffect(() => {
+    if (session) {
+      getDashboardData()
+    }
+  }, [session])
+  async function getDashboardData() {
+    const response = await fetch(`/api/get-dashboard-parts?userID=${session.user.uid}`)
+    const responseJson = await response.json()
+    setDashboardParts(responseJson)
+  }
+  if (dashboardParts) {
+    return <DashboardPage dashboardParts={dashboardParts} />
+  }
+  else {
+    return <LoadingScreen />
+  }
 };
 export default Dashboard;
