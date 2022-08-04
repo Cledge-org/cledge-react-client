f50fe202049f31f9a8bf703a7a9dd1a2d9c99d34
Chandan <chandancvp08@gmail.com>
2022-08-05 01:12:53 +0530

    made slight change in the UI

--- src/main-pages/CollegeList/CollegeListPage.tsx
+++ src/main-pages/CollegeList/CollegeListPage.tsx
@@ -5,12 +5,16 @@ import styles from './styles.module.scss'
 import TierCard from 'src/main-pages/CollegeList/components/TierCard';
 const CollegeListPage: NextApplicationPage<{ accountInfo: AccountInfo }> = ({ accountInfo }) => {
     return (
-        <div style={{marginLeft:"80px"}}>
+        <div style={{ marginLeft: "80px",marginRight: "80px"  }}>
             <div>
                 <p className={styles.myFavHeader}>My favorites</p>
             </div>
-            <TierCard name='Target Schools'/>
-            
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                <TierCard name='Target Schools' />
+                <TierCard name='Fit Schools' />
+                <TierCard name='Reach Schools' />
+            </div>
+
         </div>
     )
 }
