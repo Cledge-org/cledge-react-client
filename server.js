3481c3820ce5568ccc7e92cc40045af5ba2a657a
Ricky Liao <liaoric@uw.edu>
2023-05-03 20:42:21 -0700

    port change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { getEnvVariable } = require("./src/config/getConfig");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 8080;
+const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
