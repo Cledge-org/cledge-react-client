0fea79263081db39cf7ca9689fc0a5b21e369dd2
Gengur123 <meleearthur@gmail.com>
2023-09-11 19:27:48 -0700

    EC log WIP

--- src/main-pages/CheckInPage/CheckInPage.tsx
+++ src/main-pages/CheckInPage/CheckInPage.tsx
@@ -790,15 +790,6 @@ const CheckIn: NextApplicationPage<{
           insertionId: session.data.user.uid
         }),
       }),
-      fetch('/api/metrics/put-activities-logs', {
-        method: 'POST',
-        body: JSON.stringify({
-          userId: session.data.user.uid,
-          activities: userActivities,
-          responses: activitiesResponses,
-          insertionId: session.data.user.uid
-        }),
-      }),
       fetch('/api/metrics/put-academics', {
           method: 'POST',
           body: JSON.stringify({
